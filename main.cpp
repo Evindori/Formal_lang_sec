52db7e44203bbb88c4e8fa0086338aacc8791c06
Evindori <evindori@gmail.com>
2020-12-18 10:59:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using namespace std;
 class Algo {
 public:
 	Algo(vector<pair<char, string>> grammar, set<char> grammar_symbols, char start_symbol);
-	void fit();
+	void fit();//grammar содержится в самом algo, поэтому в ее передае нет нужды
 	bool predict(string s);
 
 private:
@@ -21,8 +21,8 @@ private:
 	vector<pair<char, string>> grammar_;
 	vector<set<element>> states_;
 	map<char, set<char>> first_;
-	map<pair<int, char>, pair<int, int>> lr_action_;
-	map<pair<int, char>, pair<int, int>> lr_step;
+	map<pair<int, char>, pair<int, int>> action_;
+	map<pair<int, char>, pair<int, int>> step_;
 	set<element> step(const set<element>& state, char c);
 	set<char> grammar_symbols_;
 
@@ -42,9 +42,7 @@ Algo::Algo(vector<pair<char, string>> grammar, set<char> grammar_symbols, char s
 
 void Algo::first_compution() {
 	bool not_enogh = true;
-	for (auto symbol : grammar_symbols_) {
-		first_[symbol] = set<char>();
-	}
+	for (auto symbol : grammar_symbols_) {first_[symbol] = set<char>();}
 	while (not_enogh) {
 		not_enogh = false;
 		for (auto rule : grammar_) {
@@ -53,30 +51,22 @@ void Algo::first_compution() {
 			set<char> prev_set = first_[first_r];
 			if (second_r == "") {
 				first_[first_r].insert('\0');
-				if (prev_set != first_[first_r]) {
-					not_enogh = true;
-				}
+				if (prev_set != first_[first_r]) {not_enogh = true;}
 				continue;
 			}
 			if (grammar_symbols_.find(second_r[0]) == grammar_symbols_.end()) {
 				first_[first_r].insert(second_r[0]);
-				if (prev_set != first_[first_r]) {
-					not_enogh = true;
-				}
+				if (prev_set != first_[first_r]) {not_enogh = true;}
 				continue;
 			}
 			for (auto c : first_[second_r[0]]) {
-				if (c != '\0') {
-					first_[first_r].insert(c);
-				}
+				if (c != '\0') {first_[first_r].insert(c);}
 			}
 			int i = 0;
-			while (i < second_r.length() - 1 && first_[second_r[i]].count('\0')) {
+			while (first_[second_r[i]].count('\0') && i < second_r.length() - 1) {
 				if (grammar_symbols_.find(second_r[i + 1]) != grammar_symbols_.end()) {
 					for (auto c : first_[second_r[i + 1]]) {
-						if (c != '\0') {
-							first_[first_r].insert(c);
-						}
+						if (c != '\0') {first_[first_r].insert(c);}
 					}
 					i++;
 				}
@@ -86,14 +76,10 @@ void Algo::first_compution() {
 				}
 			}
 			if (grammar_symbols_.find(second_r[i]) != grammar_symbols_.end()) {
-				if (i == second_r.length() - 1 && first_[second_r[i]].count('\0')) {
-					first_[first_r].insert('\0');
-				}
+				if (first_[second_r[i]].count('\0') && i == second_r.length() - 1) {first_[first_r].insert('\0');}
 			}
 
-			if (prev_set != first_[first_r]) {
-				not_enogh = true;
-			}
+			if (prev_set != first_[first_r]) {not_enogh = true;}
 		}
 	}
 }
@@ -104,12 +90,10 @@ void Algo::closing(set<Algo::element>& state) {
 		size = state.size();
 		for (auto item : state) {
 			auto curr_rule = grammar_[get<0>(item)];
-			if (curr_rule.second.length() == get<1>(item))
-				continue;
-			char B = curr_rule.second[get<1>(item)];
+			if (curr_rule.second.length() == get<1>(item)) { continue; }
 			for (int i = 0; i < grammar_.size(); i++) {
 				auto rule = grammar_[i];
-				if (rule.first == B) {
+				if (rule.first == curr_rule.second[get<1>(item)]) {
 					if (curr_rule.second.length() == get<1>(item) + 1) {
 						state.insert(make_tuple(i, 0, get<2>(item)));
 						continue;
@@ -118,9 +102,7 @@ void Algo::closing(set<Algo::element>& state) {
 						state.insert(make_tuple(i, 0, curr_rule.second[get<1>(item) + 1]));
 						continue;
 					}
-					for (auto c : first_[curr_rule.second[get<1>(item) + 1]]) {
-						state.insert(make_tuple(i, 0, c));
-					}
+					for (auto c : first_[curr_rule.second[get<1>(item) + 1]]) {state.insert(make_tuple(i, 0, c));}
 				}
 			}
 		}
@@ -131,13 +113,8 @@ set<Algo::element> Algo::step(const set<Algo::element>& state, char c) {
 	set<Algo::element> new_state;
 	for (auto item : state) {
 		auto curr_rule = grammar_[get<0>(item)];
-		if (curr_rule.second.length() == get<1>(item)) {
-			continue;
-		}
-		char B = curr_rule.second[get<1>(item)];
-		if (B == c) {
-			new_state.insert(make_tuple(get<0>(item), get<1>(item) + 1, get<2>(item)));
-		}
+		if (curr_rule.second.length() == get<1>(item)) {continue;}
+		if (curr_rule.second[get<1>(item)] == c) {new_state.insert(make_tuple(get<0>(item), get<1>(item) + 1, get<2>(item)));}
 	}
 	closing(new_state);
 	return new_state;
@@ -147,29 +124,25 @@ set<Algo::element> Algo::step(const set<Algo::element>& state, char c) {
 void Algo::grammar_check(int i) {
 	for (auto rule : states_[i]) {
 		if (grammar_[get<0>(rule)].first == start_symbol_ && get<1>(rule) == 1) {
-			if (lr_action_.find(make_pair(i, '\0')) != lr_action_.end()) {
-				if (lr_action_[make_pair(i, '\0')] != make_pair(2, -1))
-					throw logic_error("Wrong grammar");
+			if (action_.find(make_pair(i, '\0')) != action_.end()) {
+				if (action_[make_pair(i, '\0')] != make_pair(2, -1)) {throw logic_error("Wrong grammar");}
 			}
-			else
-				lr_action_[make_pair(i, '\0')] = make_pair(2, -1);
+			else {action_[make_pair(i, '\0')] = make_pair(2, -1);}
 		}
 	}
 	for (char c = -127; c < 127; c++) {
 		for (auto rule : states_[i]) {
 			if (get<1>(rule) < grammar_[get<0>(rule)].second.length()) {
 				char a = grammar_[get<0>(rule)].second[get<1>(rule)];
-				if (a != c)
-					continue;
+				if (a != c) { continue;}
 				if (!grammar_symbols_.count(a)) {
 					for (int j = 0; j < states_.size(); j++) {
 						if (step(states_[i], a) == states_[j]) {
-							if (lr_action_.find(make_pair(i, a)) != lr_action_.end()) {
-								if (lr_action_[make_pair(i, a)] != make_pair(0, j))
-									throw logic_error("Wrong grammar");
+							if (action_.find(make_pair(i, a)) != action_.end()) {
+								if (action_[make_pair(i, a)] != make_pair(0, j)) { throw logic_error("Wrong grammar");}
 							}
 							else {
-								lr_action_[make_pair(i, a)] = make_pair(0, j);
+								action_[make_pair(i, a)] = make_pair(0, j);
 								break;
 							}
 						}
@@ -180,18 +153,13 @@ void Algo::grammar_check(int i) {
 			}
 			else {
 				char a = get<2>(rule);
-				if (a != c)
-					continue;
-				if (grammar_[get<0>(rule)].first == start_symbol_)
-					continue;
+				if (a != c) {continue;}
+				if (grammar_[get<0>(rule)].first == start_symbol_) {continue;}
 				if (!grammar_symbols_.count(a)) {
-					if (lr_action_.find(make_pair(i, a)) != lr_action_.end()) {
-						if (lr_action_[make_pair(i, a)] != make_pair(1, get<0>(rule)))
-							throw logic_error("Wrong grammar");
+					if (action_.find(make_pair(i, a)) != action_.end()) {
+						if (action_[make_pair(i, a)] != make_pair(1, get<0>(rule))) { throw logic_error("Wrong grammar");}
 					}
-					else {
-						lr_action_[make_pair(i, a)] = make_pair(1, get<0>(rule));
-					}
+					else { action_[make_pair(i, a)] = make_pair(1, get<0>(rule)); }
 				}
 			}
 		}
@@ -203,9 +171,7 @@ void Algo::fit() {
 	first_compution();
 	for (int i = 0; i < grammar_.size(); i++) {
 		auto r = grammar_[i];
-		if (r.first == start_symbol_) {
-			start.insert(make_tuple(i, 0, '\0'));
-		}
+		if (r.first == start_symbol_) {start.insert(make_tuple(i, 0, '\0'));}
 	}
 	closing(start);
 	states_.push_back(start);
@@ -215,9 +181,7 @@ void Algo::fit() {
 			auto state = step(curr_state, c);
 			if (!state.empty()) {
 				for (auto s : states_) {
-					if (s == state) {
-						return;
-					}
+					if (s == state) {return;}
 				}
 				states_.push_back(state);
 			}
@@ -227,21 +191,15 @@ void Algo::fit() {
 	for (int i = 0; i < states_.size(); i++) {
 		grammar_check(i);
 		for (char c = -127; c < 127; c++) {
-			if (lr_action_.find(make_pair(i, c)) == lr_action_.end()) {
-				lr_action_[make_pair(i, c)] = make_pair(3, -1);
-			}
+			if (action_.find(make_pair(i, c)) == action_.end()) {action_[make_pair(i, c)] = make_pair(3, -1);}
 		}
 	}
 	for (int i = 0; i < states_.size(); i++) {
 		for (auto a : grammar_symbols_) {
 			for (int j = 0; j < states_.size(); j++) {
-				if (step(states_[i], a) == states_[j]) {
-					lr_step[make_pair(i, a)] = make_pair(0, j);
-				}
+				if (step(states_[i], a) == states_[j]) {step_[make_pair(i, a)] = make_pair(0, j);}
 			}
-			if (lr_step.find(make_pair(i, a)) == lr_step.end()) {
-				lr_step[make_pair(i, a)] = make_pair(1, -1);
-			}
+			if (step_.find(make_pair(i, a)) == step_.end()) {step_[make_pair(i, a)] = make_pair(1, -1);}
 		}
 	}
 }
@@ -252,34 +210,24 @@ bool Algo::predict(string s) {
 	s += '\0';
 	helping_stack.push_back(make_pair(0, '\0'));
 	while (position <= s.length()) {
-		char a = s[position];
-		pair<int, char> t = helping_stack.back();
-		if (lr_action_[make_pair(t.first, a)].first == 0) {
+		char item = s[position];
+		pair<int, char> back_ = helping_stack.back();
+		if (action_[make_pair(back_.first, item)].first == 0) {
+			helping_stack.push_back(make_pair(action_[make_pair(back_.first, item)].second, item));
 			position++;
-			helping_stack.push_back(make_pair(lr_action_[make_pair(t.first, a)].second, a));
 			continue;
 		}
-		if (lr_action_[make_pair(t.first, a)].first == 1) {
-			pair<char, string> rule = grammar_[lr_action_[make_pair(t.first, a)].second];
-			for (int i = 0; i < rule.second.length(); i++) {
-				helping_stack.pop_back();
-			}
+		if (action_[make_pair(back_.first, item)].first == 1) {
+			pair<char, string> rule = grammar_[action_[make_pair(back_.first, item)].second];
+			for (int i = 0; i < rule.second.length(); i++) {helping_stack.pop_back();}
 			int q_new = helping_stack.back().first;
 			char first_r = rule.first;
-			if (lr_step[make_pair(q_new, first_r)].first == 0) {
-				helping_stack.push_back(make_pair(lr_step[make_pair(q_new, first_r)].second, first_r));
-			}
-			else {
-				break;
-			}
+			if (step_[make_pair(q_new, first_r)].first == 0) {helping_stack.push_back(make_pair(step_[make_pair(q_new, first_r)].second, first_r));}
+			else {break;}
 			continue;
 		}
-		if (lr_action_[make_pair(t.first, a)].first == 2) {
-			return true;
-		}
-		if (lr_action_[make_pair(t.first, a)].first == 3) {
-			break;
-		}
+		if (action_[make_pair(back_.first, item)].first == 2) {return true;}
+		if (action_[make_pair(back_.first, item)].first == 3) {break;}
 	}
 	return false;
 }
@@ -289,17 +237,16 @@ void ans(Algo parser,int cont ) {
 		string word;
 		getline(cin, word);
 		bool result = parser.predict(word);
-		if (result)
-			cout << "Correct";
-		else
-			cout << "Incorrect";
+		if (result) { cout << "Correct"; }
+		else { cout << "Incorrect"; }
 	}
 }
 
 int main(int argc, char* argv[]) {
 	int cont;
 	char start_symbol;
-	string file, symbols, rule;
+	char left;
+	string file, symbols, rule, right;
 	string::size_type sz;
 	vector<pair<char, string>> grammar;
 	set<char> grammar_symbols;
@@ -314,16 +261,12 @@ int main(int argc, char* argv[]) {
 
 	while (!input.eof()) {
 		input >> rule;
-		char left = rule[0];
-		string right = rule.substr(3);
+		left = rule[0];
+		right = rule.substr(3);
 		grammar.push_back(make_pair(left, right));
 	}
-	for (auto c : symbols) {
-		grammar_symbols.insert(c);
-	}
+	for (auto c : symbols) {grammar_symbols.insert(c);}
 	Algo parser = Algo(grammar, grammar_symbols, start_symbol);
 	parser.fit();
-	string empty;
-	getline(cin, empty);
 	ans(parser, cont);
 }
